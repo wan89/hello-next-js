dc1d610b8a047ad1711cd037b5210dc18890c447
wan.b <47207387+wan89@users.noreply.github.com>
2023-07-07 13:05:14 +0900

    [front/fix] 라우팅 테스트, 기초 문법 확인

---
+++ hello/src/app/list/a/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function a() {
+    let name = 'a';
+  
+    return (
+      <div>
+        <h2>aaa</h2>
+        <a></a>
+      </div>
+    )
+  }
+  
+  
---
+++ hello/src/app/list/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function ListPage() {
+    let name = 'park';
+  
+    return (
+      <div>
+        <h2>list페이지</h2>
+        <a></a>
+      </div>
+    )
+  }
+  
+  
